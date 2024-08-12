54540c1460741c1cb8f78b5b13f195d99a566a2b
gashnick <gashnicolas@gmail.com>
2024-08-12 10:52:57 +0200

    Create portifolio project and initialise files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { Header, About, Links, Projects, Client, Footer } from "./components";
+
+import { Navbar } from "./containers";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="white_bg">
+        <Navbar />
+        <Header />
+      </div>
+      <About />
+      <Links />
+      <Projects />
+      <Client />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Header } from "./header/Header";
+export { default as About } from "./about/About";
+export { default as Links } from "./links/Links";
+export { default as Projects } from "./projects/Projects";
+export { default as Client } from "./clients/Client";
